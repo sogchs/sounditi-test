c5fa3ea39011a18f0e819f54f467f262023cbfd6
sogchs <sogchs@gmail.com>
2019-05-14 09:48:26 +0200

    load lottie header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,49 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Lottie from 'react-lottie'
+import headerAnimation from './after-effects/4031-voice-recognition.json'
+import playAnimation from './after-effects/Play-Pause.json'
 import './App.css';
 
-function App() {
+class App extends Component {
+
+render() {
+  const headerOptions = {
+    loop: true,
+    autoplay: true, 
+    animationData: headerAnimation,
+    rendererSettings: {
+      preserveAspectRatio: 'xMidYMid slice'
+    }
+  };
+
+  const playOptions = { 
+    loop: false,
+    autoplay: false, 
+    animationData: playAnimation,
+    rendererSettings: {
+      preserveAspectRatio: 'xMidYMid slice'
+    }
+  };
+
   return (
     <div className="App">
-      Hola que ase
+      <div className="header">
+        <p>welcome to</p>
+        <h1>SOUNDITI</h1>
+        <Lottie options={headerOptions}
+                height={400}
+                width={1000}
+          />
+      </div>
+      <div className="">
+        <Lottie options={playOptions}
+              height={50}
+              width={50}
+        />
+      </div>
+      
     </div>
   );
-}
+}}
 
 export default App;
