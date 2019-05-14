5c585f8aeaccc86ed92362e93924dd0a0fdd246a
sogchs <sogchs@gmail.com>
2019-05-14 10:44:18 +0200

    Two lottie in proyect

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
 import React, { Component } from 'react';
 import Lottie from 'react-lottie'
 import headerAnimation from './after-effects/4031-voice-recognition.json'
-import playAnimation from './after-effects/Play-Pause.json'
+import pollitoAnimation from './after-effects/pollito.json'
 import './App.css';
 
 class App extends Component {
+  state = { 
+    isPaused: false,
+    playMusic: false,
+    pauseMusic: true
+  }
+
+  url = "http://streaming.tdiradio.com:8000/house.mp3"
+  audio = new Audio(this.url);
+  play = () => {
+    this.setState({
+      playMusic: true,
+      pauseMusic: false,
+      isPaused: false 
+    });
+    console.log(this.audio);
+    this.audio.play();
+  }
+
+  pause = () => {
+    this.setState({ playMusic: false, pauseMusic: true, isPaused: !this.state.isPaused });
+    this.audio.pause();
+  }
+  
+  
+
 
 render() {
   const headerOptions = {
@@ -16,14 +41,16 @@ render() {
     }
   };
 
-  const playOptions = { 
-    loop: false,
+  const pollitoOptions = { 
+    loop: true,
     autoplay: false, 
-    animationData: playAnimation,
+    animationData: pollitoAnimation,
     rendererSettings: {
-      preserveAspectRatio: 'xMidYMid slice'
+      preserveAspectRatio: 'xMidYMid slice',
     }
   };
+
+  
 
   return (
     <div className="App">
@@ -31,17 +58,22 @@ render() {
         <p>welcome to</p>
         <h1>SOUNDITI</h1>
         <Lottie options={headerOptions}
-                height={400}
+                height={300}
                 width={1000}
           />
       </div>
-      <div className="">
-        <Lottie options={playOptions}
-              height={50}
-              width={50}
+      <div className="content">
+      <div>
+        <button  onClick={this.play}>PLAY</button>
+        <button  onClick={this.pause}>PAUSE</button>
+      </div>
+        <Lottie options={pollitoOptions}
+          height={200}
+          width={200}
+          isPaused={this.state.isPaused}
+          sound={this.state.sound}
         />
       </div>
-      
     </div>
   );
 }}
